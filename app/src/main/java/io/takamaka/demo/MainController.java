dff255b81e89966275681a7c63e00f24a62d6ec8
isacco.borsani@h2tcoin.com <isacco.borsani@h2tcoin.com>
2020-10-19 18:18:49 +0200

    Fix commit

---
+++ app/src/main/java/io/takamaka/demo/MainController.java
@@ -0,0 +1,15 @@
+package io.takamaka.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
