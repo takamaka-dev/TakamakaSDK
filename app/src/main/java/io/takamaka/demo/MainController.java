be239e403185ca73567c271b370379fe27696d9f
isacco.borsani@h2tcoin.com <isacco.borsani@h2tcoin.com>
2020-10-20 10:15:12 +0200

    some fix

--- app/src/main/java/io/takamaka/demo/MainController.java
+++ app/src/main/java/io/takamaka/demo/MainController.java
@@ -13,8 +13,5 @@ public class MainController extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setBackgroundColor(Color.BLUE);
     }
 }
