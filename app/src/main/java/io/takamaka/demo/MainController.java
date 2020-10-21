e61953592bb4393da3f52acac1eb6707bc5d84be
isacco.borsani@h2tcoin.com <isacco.borsani@h2tcoin.com>
2020-10-21 16:09:55 +0200

    some fix

--- app/src/main/java/io/takamaka/demo/MainController.java
+++ app/src/main/java/io/takamaka/demo/MainController.java
@@ -11,11 +11,11 @@ public class MainController extends AppCompatActivity {
 
     // Make sure to use the FloatingActionButton
     // for all the FABs
-    FloatingActionButton mAddFab, mAddAlarmFab, mAddPersonFab, createWalletFab, restoreWalletFab;
+    FloatingActionButton takamakaButton, loginButton, tokensButton, createWalletFab, restoreWalletFab;
 
     // These are taken to make visible and invisible along
     // with FABs
-    TextView addAlarmActionText, addPersonActionText, createWalletText, restoreWalletText;
+    TextView takamakaButtonText, loginButtonText, createWalletText, restoreWalletText;
 
     // to check whether sub FAB buttons are visible or not.
     Boolean isAllFabsVisible;
@@ -27,27 +27,27 @@ public class MainController extends AppCompatActivity {
 
         // Register all the FABs with their IDs
         // This FAB button is the Parent
-        mAddFab = findViewById(R.id.add_fab);
+        takamakaButton = findViewById(R.id.takamaka_button);
         // FAB button
-        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
-        mAddPersonFab = findViewById(R.id.add_person_fab);
+        loginButton = findViewById(R.id.login_button);
+        tokensButton = findViewById(R.id.tokens_button);
         createWalletFab = findViewById(R.id.create_wallet);
         restoreWalletFab = findViewById(R.id.restore_wallet);
         // Also register the action name text, of all the FABs.
-        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
-        addPersonActionText = findViewById(R.id.add_person_action_text);
+        takamakaButtonText = findViewById(R.id.login_button_text);
+        loginButtonText = findViewById(R.id.tokens_button_text);
         createWalletText = findViewById(R.id.create_wallet_text);
         restoreWalletText = findViewById(R.id.restore_wallet_text);
 
 
         // Now set all the FABs and all the action name
         // texts as GONE
-        mAddAlarmFab.setVisibility(View.GONE);
-        mAddPersonFab.setVisibility(View.GONE);
+        loginButton.setVisibility(View.GONE);
+        tokensButton.setVisibility(View.GONE);
         createWalletFab.setVisibility(View.GONE);
         restoreWalletFab.setVisibility(View.GONE);
-        addAlarmActionText.setVisibility(View.GONE);
-        addPersonActionText.setVisibility(View.GONE);
+        takamakaButtonText.setVisibility(View.GONE);
+        loginButtonText.setVisibility(View.GONE);
         createWalletText.setVisibility(View.GONE);
         restoreWalletText.setVisibility(View.GONE);
 
@@ -60,7 +60,7 @@ public class MainController extends AppCompatActivity {
         // visible only when Parent FAB button is clicked So
         // we have to handle the Parent FAB button first, by
         // using setOnClickListener you can see below
-        mAddFab.setOnClickListener(
+        takamakaButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -69,12 +69,12 @@ public class MainController extends AppCompatActivity {
                             // when isAllFabsVisible becomes
                             // true make all the action name
                             // texts and FABs VISIBLE.
-                            mAddAlarmFab.show();
-                            mAddPersonFab.show();
+                            loginButton.show();
+                            tokensButton.show();
                             createWalletFab.show();
                             restoreWalletFab.show();
-                            addAlarmActionText.setVisibility(View.VISIBLE);
-                            addPersonActionText.setVisibility(View.VISIBLE);
+                            takamakaButtonText.setVisibility(View.VISIBLE);
+                            loginButtonText.setVisibility(View.VISIBLE);
                             createWalletText.setVisibility(View.VISIBLE);
                             restoreWalletText.setVisibility(View.VISIBLE);
 
@@ -87,12 +87,12 @@ public class MainController extends AppCompatActivity {
                             // when isAllFabsVisible becomes
                             // true make all the action name
                             // texts and FABs GONE.
-                            mAddAlarmFab.hide();
-                            mAddPersonFab.hide();
+                            loginButton.hide();
+                            tokensButton.hide();
                             createWalletFab.hide();
                             restoreWalletFab.hide();
-                            addAlarmActionText.setVisibility(View.GONE);
-                            addPersonActionText.setVisibility(View.GONE);
+                            takamakaButtonText.setVisibility(View.GONE);
+                            loginButtonText.setVisibility(View.GONE);
                             createWalletText.setVisibility(View.GONE);
                             restoreWalletText.setVisibility(View.GONE);
 
@@ -108,11 +108,11 @@ public class MainController extends AppCompatActivity {
         // FAB. Here it shows simple Toast msg. The Toast
         // will be shown only when they are visible and only
         // when user clicks on them
-        mAddPersonFab.setOnClickListener(
+        tokensButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Toast.makeText(MainController.this, "Person Added", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainController.this, "Send tokens section", Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -120,11 +120,11 @@ public class MainController extends AppCompatActivity {
         // FAB. Here it shows simple Toast msg The Toast
         // will be shown only when they are visible and only
         // when user clicks on them
-        mAddAlarmFab.setOnClickListener(
+        loginButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Toast.makeText(MainController.this, "Alarm Added", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainController.this, "Login section", Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -133,7 +133,7 @@ public class MainController extends AppCompatActivity {
 
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(MainController.this, "Create a Wallet section", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainController.this, "Create Wallet section", Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -142,7 +142,7 @@ public class MainController extends AppCompatActivity {
 
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(MainController.this, "Restore a Wallet section", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainController.this, "Restore Wallet section", Toast.LENGTH_SHORT).show();
                     }
                 }
         );
