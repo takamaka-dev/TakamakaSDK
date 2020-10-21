6468b10701ca566c61f36e146ca3f0e5209f9288
isacco.borsani@h2tcoin.com <isacco.borsani@h2tcoin.com>
2020-10-21 15:24:58 +0200

    some fix

--- app/src/main/java/io/takamaka/demo/MainController.java
+++ app/src/main/java/io/takamaka/demo/MainController.java
@@ -1,17 +1,151 @@
 package io.takamaka.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Button;
-
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainController extends AppCompatActivity {
+
+    // Make sure to use the FloatingActionButton
+    // for all the FABs
+    FloatingActionButton mAddFab, mAddAlarmFab, mAddPersonFab, createWalletFab, restoreWalletFab;
+
+    // These are taken to make visible and invisible along
+    // with FABs
+    TextView addAlarmActionText, addPersonActionText, createWalletText, restoreWalletText;
+
+    // to check whether sub FAB buttons are visible or not.
+    Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Register all the FABs with their IDs
+        // This FAB button is the Parent
+        mAddFab = findViewById(R.id.add_fab);
+        // FAB button
+        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
+        mAddPersonFab = findViewById(R.id.add_person_fab);
+        createWalletFab = findViewById(R.id.create_wallet);
+        restoreWalletFab = findViewById(R.id.restore_wallet);
+        // Also register the action name text, of all the FABs.
+        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
+        addPersonActionText = findViewById(R.id.add_person_action_text);
+        createWalletText = findViewById(R.id.create_wallet_text);
+        restoreWalletText = findViewById(R.id.restore_wallet_text);
+
+
+        // Now set all the FABs and all the action name
+        // texts as GONE
+        mAddAlarmFab.setVisibility(View.GONE);
+        mAddPersonFab.setVisibility(View.GONE);
+        createWalletFab.setVisibility(View.GONE);
+        restoreWalletFab.setVisibility(View.GONE);
+        addAlarmActionText.setVisibility(View.GONE);
+        addPersonActionText.setVisibility(View.GONE);
+        createWalletText.setVisibility(View.GONE);
+        restoreWalletText.setVisibility(View.GONE);
+
+
+        // make the boolean variable as false, as all the
+        // action name texts and all the sub FABs are invisible
+        isAllFabsVisible = false;
+
+        // We will make all the FABs and action name texts
+        // visible only when Parent FAB button is clicked So
+        // we have to handle the Parent FAB button first, by
+        // using setOnClickListener you can see below
+        mAddFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!isAllFabsVisible) {
+
+                            // when isAllFabsVisible becomes
+                            // true make all the action name
+                            // texts and FABs VISIBLE.
+                            mAddAlarmFab.show();
+                            mAddPersonFab.show();
+                            createWalletFab.show();
+                            restoreWalletFab.show();
+                            addAlarmActionText.setVisibility(View.VISIBLE);
+                            addPersonActionText.setVisibility(View.VISIBLE);
+                            createWalletText.setVisibility(View.VISIBLE);
+                            restoreWalletText.setVisibility(View.VISIBLE);
+
+                            // make the boolean variable true as
+                            // we have set the sub FABs
+                            // visibility to GONE
+                            isAllFabsVisible = true;
+                        } else {
+
+                            // when isAllFabsVisible becomes
+                            // true make all the action name
+                            // texts and FABs GONE.
+                            mAddAlarmFab.hide();
+                            mAddPersonFab.hide();
+                            createWalletFab.hide();
+                            restoreWalletFab.hide();
+                            addAlarmActionText.setVisibility(View.GONE);
+                            addPersonActionText.setVisibility(View.GONE);
+                            createWalletText.setVisibility(View.GONE);
+                            restoreWalletText.setVisibility(View.GONE);
+
+                            // make the boolean variable false
+                            // as we have set the sub FABs
+                            // visibility to GONE
+                            isAllFabsVisible = false;
+                        }
+                    }
+                });
+
+        // below is the sample action to handle add person
+        // FAB. Here it shows simple Toast msg. The Toast
+        // will be shown only when they are visible and only
+        // when user clicks on them
+        mAddPersonFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainController.this, "Person Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // below is the sample action to handle add alarm
+        // FAB. Here it shows simple Toast msg The Toast
+        // will be shown only when they are visible and only
+        // when user clicks on them
+        mAddAlarmFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainController.this, "Alarm Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        createWalletFab.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainController.this, "Create a Wallet section", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        restoreWalletFab.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainController.this, "Restore a Wallet section", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
     }
 }
