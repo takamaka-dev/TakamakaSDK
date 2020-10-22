5a01561cd9c6e10b0d927d2cd56e4d3d2f187293
isacco.borsani@h2tcoin.com <isacco.borsani@h2tcoin.com>
2020-10-22 11:12:42 +0200

    some fix

---
+++ app/src/main/java/io/takamaka/sdk/wallet/InstanceWalletKeystoreInterface.java
@@ -0,0 +1,40 @@
+package io.takamaka.sdk.wallet;
+
+import com.h2tcoin.takamakachain.exceptions.wallet.WalletException;
+import com.h2tcoin.takamakachain.globalContext.KeyContexts;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+
+
+public interface InstanceWalletKeystoreInterface extends Comparable<InstanceWalletKeystoreInterface> {
+    /**
+     *
+     * @return the wallet cypher
+     */
+    KeyContexts.WalletCypher getWalletCypher();
+
+    /**
+     * @param i
+     * @return the key pair public and private address related to the index
+     * @throws WalletException
+     */
+    AsymmetricCipherKeyPair getKeyPairAtIndex(int i) throws WalletException;
+
+    /**
+     * @param i
+     * @return the public key as byte array
+     * @throws WalletException
+     */
+    byte[] getPublicKeyAtIndexByte(int i) throws WalletException;
+
+    /**
+     * @param i
+     * @return the public key as URL64 image String
+     * @throws WalletException
+     */
+    String getPublicKeyAtIndexURL64(int i) throws WalletException;
+
+    /**
+     * @return the current wallet ID
+     */
+    String getCurrentWalletID();
+}
