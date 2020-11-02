2504bca3313da1183b544c2433ab1df6a312b36b
isacco.borsani@h2tcoin.com <isacco.borsani@h2tcoin.com>
2020-11-02 16:38:20 +0100

    Fix

---
+++ app/src/main/java/io/takamaka/demo/TransactionInfoActivity.java
@@ -0,0 +1,14 @@
+package io.takamaka.demo;
+
+import android.os.Bundle;
+
+public class TransactionInfoActivity extends MainController {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_info);
+
+
+    }
+}
