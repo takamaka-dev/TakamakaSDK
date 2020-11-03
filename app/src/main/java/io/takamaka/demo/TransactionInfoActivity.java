742dcc4f72a262ed15b425e8db2cfc68c95d53cf
elton <francesco.pasetto@h2tcoin.com>
2020-11-03 11:43:46 +0100

    TransactionInfoActivity

--- app/src/main/java/io/takamaka/demo/TransactionInfoActivity.java
+++ app/src/main/java/io/takamaka/demo/TransactionInfoActivity.java
@@ -1,7 +1,9 @@
 package io.takamaka.demo;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import io.takamaka.demo.utils.SWTracker;
@@ -9,6 +11,8 @@ import io.takamaka.demo.utils.SWTracker;
 public class TransactionInfoActivity extends MainController {
 
     private TextView textViewResult;
+    private Button buttonGoHome;
+    private Button buttonNewTransaction;
 
 
     @Override
@@ -17,6 +21,22 @@ public class TransactionInfoActivity extends MainController {
         setContentView(R.layout.activity_transaction_info);
 
         textViewResult = findViewById(R.id.textViewTransactionStatus);
+        buttonGoHome = findViewById(R.id.button_go_home);
+        buttonNewTransaction = findViewById(R.id.button_new_transaction);
+
+        //action button go home  linked
+        buttonGoHome.setOnClickListener(e -> {
+            Intent activityHome = new Intent(getApplicationContext(), HomeWalletActivity.class);
+            startActivity(activityHome);
+
+        });
+        //action button transaction  linked
+        buttonNewTransaction.setOnClickListener( e -> {
+            Intent activitySendToken = new Intent(getApplicationContext(), SendTokenActivity.class);
+            startActivity(activitySendToken);
+        });
+
+
         if(SWTracker.i().isTransactionResult() == true){
 
             textViewResult.setText("Transaction Successfully Transmitted");
