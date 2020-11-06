065d7a64d8d0f8417eeddecb5c5d1cf1ea056a36
elton <francesco.pasetto@h2tcoin.com>
2020-11-06 16:42:42 +0100

    TransactionInfoActivity

--- app/src/main/java/io/takamaka/demo/SettingsActivity.java
+++ app/src/main/java/io/takamaka/demo/SettingsActivity.java
@@ -1,13 +1,16 @@
 package io.takamaka.demo;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import io.takamaka.demo.utils.SWTracker;
 
 public class SettingsActivity extends MainController {
 
+    Spinner spinnerButton;
     Button radioButtonProduction, radioButtonTest;
     RadioGroup radioGroup;
 
@@ -16,7 +19,15 @@ public class SettingsActivity extends MainController {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         initMenu();
+
+
+
+
         setCurrentActivity(this);
+
+
+
+
         radioButtonProduction = findViewById(R.id.radioButtonProduction);
         radioButtonTest = findViewById(R.id.radioButtonTest);
         radioGroup = findViewById(R.id.radioGroup);
@@ -39,6 +50,28 @@ public class SettingsActivity extends MainController {
                     SWTracker.i().setCurrentSetting("test");
                 }
         );
+
+
+        spinnerButton = findViewById(R.id.spinner_button);
+
+        int childCount = spinnerButton.getChildCount();
+        for (int sIndex = 1; sIndex < childCount; sIndex++) {
+
+            View v = spinnerButton.getChildAt(sIndex);
+            v.setOnClickListener(e-> {
+
+                if(v.getTag().equals("test_env")){
+                    SWTracker.i().setCurrentSetting("test");
+
+                } else if(v.getTag().equals("prod_env")) {
+                    SWTracker.i().setCurrentSetting("prod");
+
+
+                    }
+
+            });
+
+        }
     }
 
 
